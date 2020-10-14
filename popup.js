b53599ea6fb4555dbe3d47fe84f89e061f29c79c
kanchenhao <azstaylorswift13@gmail.com>
2020-10-14 17:30:23 +0800

    some bugs have been fixed

--- popup.js
+++ popup.js
@@ -1,67 +1,91 @@
 ﻿window.onload = onWindowLoad;
 
 function onWindowLoad() {
-  var message = document.querySelector('#message');
-  chrome.tabs.executeScript(null, {
-    file: "getPagesSource.js"
-  }, function () {
-    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-    if (chrome.runtime.lastError) {
-      // message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-      message.innerText = 'Not available in this website!';
+  var message = document.querySelector("#message");
+  chrome.tabs.executeScript(
+    null,
+    {
+      file: "getPagesSource.js",
+    },
+    function () {
+      // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+      if (chrome.runtime.lastError) {
+        // message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+        message.innerText = "Not available in this website!";
+      }
     }
-  });
+  );
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender) {
   if (request.action == "getSource") {
-    message.innerText = '';
+    message.innerText = "";
     var reqs = request.source;
-    // console.log(reqs);
-    if (reqs == '0') {
-      message.innerText = 'No book can be downloaded!';
+    if (reqs == "0") {
+      message.innerText = "No book can be downloaded!";
     } else {
       doAll(reqs);
 
       function getBlob(urlIn, domIn) {
-        return new Promise(resolve => {
+        return new Promise((resolve) => {
           getInnerBlob(urlIn, domIn);
 
-          function getInnerBlob(url, dom, fileType = '.pdf') {
+          function getInnerBlob(url, dom, fileType = ".pdf") {
             let that = this;
             let xhr = new XMLHttpRequest();
-
-            xhr.open('GET', url, true);
-            xhr.responseType = 'blob';
+            let urlSplit = url.split("/");
+            let bookId =
+              "&bookId=" + urlSplit[urlSplit.length - 1].split(".")[0];
+            xhr.open("GET", url + bookId, true);
+            xhr.responseType = "blob";
             xhr.send();
 
             let dlProgress = document.createElement("div");
-            dlProgress.setAttribute('class', 'dl-progress');
+            dlProgress.setAttribute("class", "dl-progress");
             let spanDiv = document.createElement("span");
             dlProgress.appendChild(spanDiv);
             dom.appendChild(dlProgress);
 
             // Monitor download progress events
-            xhr.addEventListener("progress", function (e) {
-              if (e.lengthComputable) {
-                let percentComplete = e.loaded / e.total;
-                that.percentage = percentComplete * 100;
-                spanDiv.style.width = that.percentage + "%";
-              }
-            }, false);
+            xhr.addEventListener(
+              "progress",
+              function (e) {
+                // if (e.lengthComputable) {
+                //   let percentComplete = e.loaded / e.total;
+                //   console.log(percentComplete)
+                //   that.percentage = percentComplete * 100;
+                //   spanDiv.style.width = that.percentage + "%";
+                // }
+                var eTotal;
+                if (e.loaded){
+                  if (e.currentTarget.response){
+                    eTotal = e.total;
+                  } else {
+                    eTotal = e.loaded + 10e5;
+                  }
+                  let percentComplete = e.loaded / eTotal;
+                  that.percentage = percentComplete * 100;
+                  spanDiv.style.width = that.percentage + "%";
+                }
+              }, false);
 
             xhr.onload = () => {
               if (xhr.response.size === 0) {
-                url = url.substring(0, url.length - 3) + 'epub';
-                dom.removeChild(dlProgress);
-                getInnerBlob(url, dom, ".epub");
+                console.log(url);
+                if (url.substring(url.length - 3, url.length) == "cdf") {
+                  url = url.substring(0, url.length - 3) + "epub";
+                  dom.removeChild(dlProgress);
+                  getInnerBlob(url, dom, ".epub");
+                } else {
+                  console.log("error 404");
+                }
               } else if (xhr.status === 200) {
                 // fileSize = (xhr.response.size / (1024 * 1024)).toFixed(2)
                 // console.log(fileSize + 'M');
                 resolve([xhr.response, fileType]);
               }
             };
-          };
+          }
         });
       }
 
@@ -69,12 +93,12 @@ chrome.runtime.onMessage.addListener(function (request, sender) {
         if (window.navigator.msSaveOrOpenBlob) {
           navigator.msSaveBlob(blob, filename);
         } else {
-          const link = document.createElement('a');
-          const body = document.querySelector('body');
+          const link = document.createElement("a");
+          const body = document.querySelector("body");
           link.href = window.URL.createObjectURL(blob);
           link.download = filename;
           // fix Firefox
-          link.style.display = 'none';
+          link.style.display = "none";
           body.appendChild(link);
           link.click();
           body.removeChild(link);
@@ -84,31 +108,32 @@ chrome.runtime.onMessage.addListener(function (request, sender) {
 
       function doAll(res) {
         for (let i = 0; i < res.length; i++) {
-          let resI = res[i].replace('//', '/'); // replace '//' to '/' in some case
-          let resSplit = resI.split('/');
+          let resI = res[i].replace("//", "/"); // replace '//' to '/' in some case
+          let resSplit = resI.split("/");
           // console.log(resSplit);
-          let webSite = (resSplit[1] == 'batch') ? 'itextbook' : 'iresearchbook';
-          let storePath = '/' + resSplit[2] + '/' + resSplit[3] + '.cdf'
+          let webSite = resSplit[1] == "batch" ? "itextbook" : "iresearchbook";
+          let storePath = "/" + resSplit[2] + "/" + resSplit[3] + ".cdf";
 
           let listDiv = document.createElement("div");
           listDiv.id = "book-" + (i + 1);
 
           let bookCover = document.createElement("img");
-          bookCover.setAttribute('class', 'dl-image');
-          bookCover.src = 'https://www.' + webSite + '.cn' + resI + '.jpg';
+          bookCover.setAttribute("class", "dl-image");
+          bookCover.src = "https://www." + webSite + ".cn" + resI + ".jpg";
           listDiv.appendChild(bookCover);
 
           let dlBtn = document.createElement("a");
           let saveName = resSplit[3];
-          dlBtn.className = 'dl-link';
+          dlBtn.className = "dl-link";
           // dlBtn.download = saveName;
           // dlBtn.href = 'https://www.' + webSite + '.cn/f/cdf/file?file=' + storePath
-          let downloadUrl = 'https://www.' + webSite + '.cn/f/cdf/file?file=' + storePath;
+          let downloadUrl =
+            "https://www." + webSite + ".cn/f/cdf/file?file=" + storePath;
           dlBtn.href = "javascript:void(0);";
-          dlBtn.innerText = 'Download Book';
+          dlBtn.innerText = "Download Book";
           listDiv.appendChild(dlBtn);
 
-          dlBtn.addEventListener('click', function () {
+          dlBtn.addEventListener("click", function () {
             getBlob(downloadUrl, listDiv).then(([blob, fileType]) => {
               saveAs(blob, saveName + fileType);
               listDiv.removeChild(listDiv.children[2]);
@@ -119,5 +144,5 @@ chrome.runtime.onMessage.addListener(function (request, sender) {
         }
       }
     }
-  };
+  }
 });
